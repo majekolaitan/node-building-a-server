0a4d0d7aef0de10e8c8c114ac29d7dcfc16d7ded
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-03 20:20:51 +0400

    setup body-parser

--- server-postman.js
+++ server-postman.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/profile', (req, res) => {
 	console.log(req.body)
