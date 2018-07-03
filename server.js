4480d97985bdd600a4a36dd3d32d12671840afc7
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-03 19:20:16 +0400

    205. Building a Server - send json or javascript object response

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const server = http.createServer((request, response) => {
 	console.log('method', request.method)
 	console.log('url', request.url)
 
-	response.setHeader('Content-Type', 'text/html');
-	response.end('<h1>Hellooooooo!</h1>');
+	const user = {
+		name: 'John',
+		hobby: 'Skating'
+	}
+
+	response.setHeader('Content-Type', 'application/json');
+	response.end(JSON.stringify(user));
 })
 
 server.listen(3000);
