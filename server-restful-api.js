db9f39d3cb324ca2eaab643c1da4fc8c158a0dee
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-03 20:59:58 +0400

    209. RESTful APIs - response - send html file to browser

--- server-restful-api.js
+++ server-restful-api.js
@@ -3,12 +3,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
-
-app.get('/:id', (req, res) => {
-	console.log(req.params)
-	res.status(404).send("not found");
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
