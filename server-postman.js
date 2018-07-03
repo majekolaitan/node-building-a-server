22dbb6fa711bc2656ff923016a8ab89b1c2ba47a
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-03 20:27:45 +0400

    208. Postman - fix sending json from postman

--- server-postman.js
+++ server-postman.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 app.post('/profile', (req, res) => {
 	console.log(req.body)
