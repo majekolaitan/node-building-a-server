b679115e3feb9988520a49b5316ba716df5fc509
majekolaitan@gmail.com <gVG9totEv22u>
2018-07-03 19:10:09 +0400

    205. Building a Server - use createServer response parameters - check chrome inspector network tab > header & response tab

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-	console.log('I hear you! Thanks for the request')
+const server = http.createServer((request, response) => {
+	response.setHeader('Content-Type', 'text/html');
+	response.end('<h1>Hellooooooo!</h1>');
 })
 
 server.listen(3000);
